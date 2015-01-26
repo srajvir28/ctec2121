731fa5b250c3529161d277b3835d3782fa66a6b5
Rajvir Singh <P13202994@GH584-007.lec-admin.dmu.ac.uk>
2015-01-26 11:46:31 +0000

    mathematics

---
+++ maths_branching/mathsFunction.c
@@ -0,0 +1,38 @@
+<<<<<<< HEAD
+//This is the line causing issue in the master branch
+=======
+//This is the line causing issue in the conflict branch
+>>>>>>> conflict
+
+#include <stdio.h>
+
+float add (float x, float y)
+{
+    return x + y;
+}
+
+int main()
+{
+    float a, b, r;
+    char op;
+    do {
+       printf("number  op  number?  ");
+       scanf(" %f %c %f", &a, &op, &b);
+       switch (op)
+       {
+           case '+' : r = add(a,b);
+                      break;
+           case 'q' : break;
+           default  : op='?';
+       }
+       if (op=='?')
+          printf("Unknown operator\n");
+       else if (op=='q')
+          printf("Bye\n");
+       else
+          printf("%f %c %f = %f\n", a, op, b, r);
+    }
+    while (op != 'q');
+    
+    return 0;
+}
